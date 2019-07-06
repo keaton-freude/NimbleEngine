88c3d591d440981d5afd191b1aacdf1a3377739e
Keaton Freude <freudek90@gmail.com>
2019-07-05 22:55:19 -0700

    FontAtlas being generated correctly

--- include/nimble/font-rendering/FontAtlas.h
+++ include/nimble/font-rendering/FontAtlas.h
@@ -7,13 +7,14 @@
 #include FT_FREETYPE_H
 
 #include <sstream>
+#include <vector>
 
 #include "nimble/font-rendering/Character.h"
 
 namespace Nimble {
 class FontAtlas {
 	GLuint _texture;
-	GLuint _test[128];
+	std::vector<Character> _characters;
 
 public:
 	FontAtlas() {
@@ -31,7 +32,6 @@ public:
 
 		glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 
-		Character characters[128];
 		unsigned char *buffers[128];
 
 		// Do a pass over all of the characters, storing off relevant information
@@ -41,19 +41,16 @@ public:
 				continue;
 			}
 
+			Character character;
 			// We need to figure out the width, height and offsets for every character in our atlas
-			characters[c].size = glm::ivec2({ face->glyph->bitmap.width, face->glyph->bitmap.rows });
-			characters[c].bearing = glm::ivec2({ face->glyph->bitmap_left, face->glyph->bitmap_top });
-			characters[c].advance = face->glyph->advance.x;
+			character.size = glm::ivec2({ face->glyph->bitmap.width, face->glyph->bitmap.rows });
+			character.bearing = glm::ivec2({ face->glyph->bitmap_left, face->glyph->bitmap_top });
+			character.advance = face->glyph->advance.x;
+			_characters.push_back(character);
 
 			buffers[c] = new unsigned char[face->glyph->bitmap.width * face->glyph->bitmap.rows];
 			std::memcpy(buffers[c], face->glyph->bitmap.buffer,
 						face->glyph->bitmap.width * face->glyph->bitmap.rows);
-
-			glGenTextures(1, &_test[c]);
-			glBindTexture(GL_TEXTURE_2D, _test[c]);
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, face->glyph->bitmap.width,
-						 face->glyph->bitmap.rows, 0, GL_RED, GL_UNSIGNED_BYTE, buffers[c]);
 		}
 
 		// Create a texture of size Width=Total Width of all glyphs, Height=tallest glyph
@@ -61,23 +58,24 @@ public:
 		uint16_t tallestGlyph = 0;
 		uint32_t width = 0;
 		for(GLubyte c = 0; c < 128; c++) {
-			if(characters[c].size.y > tallestGlyph) {
-				tallestGlyph = characters[c].size.y;
+			if(_characters[c].size.y > tallestGlyph) {
+				tallestGlyph = _characters[c].size.y;
 			}
-			width += characters[c].size.x;
+			width += _characters[c].size.x;
 		}
 		spdlog::info("Tallest Glyph: {}, Width: {}", tallestGlyph, width);
 
-		unsigned char *atlasBuffer = new unsigned char[tallestGlyph * width * 3];
+		unsigned char *atlasBuffer = new unsigned char[tallestGlyph * width];
+		memset(atlasBuffer, 0, width * tallestGlyph);
 
 		int new_x = 0;
 		int y = 0;
 		int accumulated_width = 0;
 
 		for(GLubyte c = 0; c < 128; c++) {
-			for(int x = 0; x < characters[c].size.x; ++x) {
-				for(int y = 0; y < characters[c].size.y; ++y) {
-					int from = (y * characters[c].size.x) + (x);
+			for(int x = 0; x < _characters[c].size.x; ++x) {
+				for(int y = 0; y < _characters[c].size.y; ++y) {
+					int from = (y * _characters[c].size.x) + (x);
 					int to = (y * width) + (x + accumulated_width);
 
 					for(int channel = 0; channel < 1; ++channel) {
@@ -86,22 +84,24 @@ public:
 				}
 			}
 
-			accumulated_width += characters[c].size.x;
+			accumulated_width += _characters[c].size.x;
 		}
 
 		glGenTextures(1, &_texture);
 		glBindTexture(GL_TEXTURE_2D, _texture);
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, width, tallestGlyph, 0, GL_RED, GL_UNSIGNED_BYTE, atlasBuffer);
 
-		// Create a buffer of the appropriate size
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		FT_Done_Face(face);
+		FT_Done_FreeType(ft);
 	}
 
 	void Test() {
 		glBindTexture(GL_TEXTURE_2D, _texture);
-
-		for(int i = 0; i < 128; ++i) {
-			glBindTexture(GL_TEXTURE_2D, _test[i]);
-		}
 	}
 };
 } // namespace Nimble
